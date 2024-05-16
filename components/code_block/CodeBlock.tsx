8a224e1481c07fb4a10f0e3f487071f31c42acf3
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-05-16 13:18:00 -0300

    make code editable if user auth

--- components/code_block/CodeBlock.tsx
+++ components/code_block/CodeBlock.tsx
@@ -4,9 +4,11 @@ import ReactCodeMirror from "@uiw/react-codemirror";
 import { useCallback, useState } from "react";
 import { typescriptLanguage } from "@codemirror/lang-javascript";
 import { tokyoNight } from "@uiw/codemirror-theme-tokyo-night";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function CodeBlock() {
   const [value, setValue] = useState("console.log('hola lucho!');");
+  const { user, error, isLoading } = useUser();
   const onChange = useCallback((val: string) => {
     setValue(val);
   }, []);
@@ -17,7 +19,8 @@ export default function CodeBlock() {
       value={value}
       height="500px"
       width="100%"
-      className="flex-1 max-w-[700px] overflow-hidden rounded-lg border border-gray-200 dark:border-gray-700"
+      editable={user ? true : false}
+      className="max-w-[700px] flex-1 overflow-hidden rounded-lg border border-gray-200 dark:border-gray-700"
       onChange={onChange}
       extensions={[typescriptLanguage]}
     />
